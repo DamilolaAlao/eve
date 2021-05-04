796b8495a38c3594fe246836e7bf276ecb316e68
Damilola Alao <oluwadamilola.alao@gmail.com>
2021-05-04 21:46:31 +0100

    notes json

--- src/app/models/note.model.js
+++ src/app/models/note.model.js
@@ -1,31 +1,10 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const NoteSchema = mongoose.Schema({
-   "coin": String,
-   "network":String,
-   "xPubKey":String,
-   "requestPrivKey":String,
-   "requestPubKey":String,
-   "copayerId":String,
-   "publicKeyRing":[
-      {
-         "xPubKey":String,
-         "requestPubKey":String
-      }
-   ],
-   "walletId":String,
-   "walletName":String,
-   "m":Number,
-   "n":Number,
-   "walletPrivKey":String,
-   "personalEncryptingKey":String,
-   "sharedEncryptingKey":String,
-   "copayerName":String,
-   "account":Number,
-   "addressType":String,
-   "version":Number,
-   "rootPath":String,
-   "keyId":String
+  notes: {
+    type: Object,
+    required: true, //depends on whether the field is mandatory or not
+  },
 });
 
-module.exports = mongoose.model('Note', NoteSchema);
+module.exports = mongoose.model("Note", NoteSchema);
