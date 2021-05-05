b445bf8f3123b4288ebbd8e7eff3b1a6d02358d9
Damilola Alao <oluwadamilola.alao@gmail.com>
2021-05-05 12:40:53 +0100

    added timestamps

--- src/app/models/note.model.js
+++ src/app/models/note.model.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
 
-const NoteSchema = mongoose.Schema({
-  notes: {
-    type: Object,
-    required: true, //depends on whether the field is mandatory or not
+const NoteSchema = mongoose.Schema(
+  {
+    notes: {
+      type: Object,
+      required: true, //depends on whether the field is mandatory or not
+    },
   },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Note", NoteSchema);
